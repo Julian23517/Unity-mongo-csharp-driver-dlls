099588a0b1abc49567a6540cd1a1633d3c6c153b
Rex Baird <fenderrex@gmail.com>
2021-05-17 13:31:15 -0700

    made a demo so its a all in one drop in as of unity 2019.4.0f1 thank you

---
+++ Model_Upload.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model_Upload : MonoBehaviour
+{
+    private const string MONGO_URI = "mongodb://127.0.0.1:27017";
+    private const string DATABASE_NAME = "testDatabase";
+    private MongoClient client;
+    private IMongoDatabase db;
+
+    void Start()
+    {
+        client = new MongoClient(MONGO_URI);
+        db = client.GetDatabase(DATABASE_NAME);
+        IMongoCollection<Model_User> userCollection = db.GetCollection<Model_User>("collectionName");
+        Model_User e = new Model_User();
+        e.name = "hope";
+        userCollection.InsertOne(e);
+        List<Model_User> userModelList = userCollection.Find(user => true).ToList();
+        Model_User[] userAsap= userModelList.ToArray();
+        foreach(Model_User asap in userAsap)
+        {
+            print(asap.name);
+        }
+    }
+
+
+}
